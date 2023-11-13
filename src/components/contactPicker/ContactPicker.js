11bff56cc6631b004e992b49110e2296ad8ad331
Daniel Campos <20570648+DCamGal94@users.noreply.github.com>
2023-11-12 19:34:26 -0600

    Implemented ContactPicker as a stateless component that renders a drop-down list of all contact names based on the following requirements: 1. Receive 4 props - The array of contacts, A callback function to handle when the onChange event is triggered, value, name. 2. Render a select element with the onChange attribute set to the callback passed in via props, a value attribute set to the value prop, and a name attribute set to the name prop. 3. Add a default option element with the text “No Contact Selected” and a value attribute of . 4. Iteratively add option elements using the contact names from the array passed in via props.

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, onChange, value, name }) => {
   return (
-    <></>
+    <select name={name} value={value} onChange={onChange}>
+      <option value="">--Please choose an option--</option>
+      {contacts.map((contact, index) => (
+        <option key={index} value={contact.name}>
+          {contact.name}
+        </option>
+      ))}
+    </select>
   );
 };
